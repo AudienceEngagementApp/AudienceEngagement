e0393297314365ce685efd39b08cc068efdbbd51
Scott Leland Crossen <scottcrossen42@gmail.com>
2018-03-23 18:55:31 -0600

    finished styles for teach home

--- project/source/app/teacher/home/TeacherHome.jsx
+++ project/source/app/teacher/home/TeacherHome.jsx
@@ -32,9 +32,18 @@ type Props = OwnProps & StateProps & DispatchProps
 class TeacherHome extends React.Component<Props>{
 
   render = (): React$Element<*> => {
-    const lessonElements = _.keys(this.props.lessons).filter((lessonId: string) => this.props.lessons[lessonId]).map((lessonId: string) => (<li key={lessonId}>
-      {this.props.lessons[lessonId].name} <Link to={`/teacher/lesson/${lessonId}`}><button>Edit</button></Link>
-    </li>))
+    const lessonElements = _.keys(this.props.lessons).filter((lessonId: string) => this.props.lessons[lessonId]).map((lessonId: string) => {
+      const numQuestions =_.keys(this.props.lessons[lessonId].questions).length
+      return (<li key={lessonId}>
+        <Link to={`/teacher/lesson/${lessonId}`} className={classnames('selection-square')}>
+          <div className={classnames('edit-lesson')}>
+            <h1>{numQuestions}</h1>
+            <h5>{numQuestions == 1 ? 'Question' : 'Questions'}</h5>
+          </div>
+          <h4>{this.props.lessons[lessonId].name}</h4>
+        </Link>
+      </li>)
+    })
     return (<div className={classnames('teacher-home')}>
       <div className={classnames('top-bar')}>
         <h1>
@@ -43,8 +52,17 @@ class TeacherHome extends React.Component<Props>{
         </h1>
       </div>
       <div className={classnames('lined-header')}><h3><span>Lessons</span></h3></div>
-      <ul>{lessonElements}</ul>
-      <Link to={`/teacher/newlesson`}><button>New lesson</button></Link>
+      <ul className={'selection-collection'}>
+        {lessonElements}
+        <li>
+          <Link to={`/teacher/newlesson`} className={classnames('selection-square')}>
+            <div className={classnames('new-lesson')}>
+              <div className={classnames('inner-icon')}>+</div>
+            </div>
+            <h4>Create New Lesson</h4>
+          </Link>
+        </li>
+      </ul>
       <Route path='/teacher/newlesson' component={() => (<div>
         <NewLessonModal {...this.props} newLesson={this.newLesson}/>
       </div>)} />
